1c69e077b85cf672564ece5f064fba05d9c965c4
coatriver <sylvain.jacot@hotmail.be>
2020-01-20 10:14:55 +1100

    Homepage created + styled & sass added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './reset.css';
+import {HomePage} from "./components/Pages/homepage";
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello</h1>
+     <HomePage/>
     </div>
   );
 }
---
+++ src/components/Pages/homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Styled from "styled-components";
+
+export class HomePage extends React.Component {
+    render() {
+        return (
+            <>
+                HomePage
+            </>
+        )
+    }
+}
