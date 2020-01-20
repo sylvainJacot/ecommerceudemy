c26a482581f99e3fde4ff97809b7efd5ec87f67b
coatriver <sylvain.jacot@hotmail.be>
2020-01-20 23:00:18 +1100

    last commit router

--- src/components/Assets/constant/HomePageNavigationData.js
+++ src/components/Assets/constant/HomePageNavigationData.js
@@ -3,6 +3,7 @@ import NavPic02 from "../img/element5-digital-ooPx1bxmTc4-unsplash.jpg";
 import NavPic03 from "../img/freestocks-org-_3Q3tsJ01nc-unsplash.jpg";
 import NavPic04 from "../img/freestocks-org-fplnXE5loWo-unsplash.jpg";
 import NavPic05 from "../img/sharon-garcia-pkUH647RogQ-unsplash.jpg";
+import TestImg from "../img/imgitem.jpg";
 
 export const homePageNavigationData = [
     {
@@ -10,12 +11,136 @@ export const homePageNavigationData = [
         itemBig: true,
         bgimg:NavPic05,
         linkUrl: 'apparels',
+        items: [
+            {
+                name: 'ArticleTest',
+                itemimg:TestImg ,
+                price: 22,
+                new: false,
+            },
+            {
+                name: 'ArticleTest',
+                itemimg:TestImg ,
+                price: 5647,
+                new: false,
+            },
+            {
+                name: 'ArticleTest',
+                itemimg:TestImg ,
+                price: 22,
+                new: false,
+            },
+            {
+                name: 'ArticleTest',
+                itemimg:TestImg ,
+                price: 22,
+                new: false,
+            },
+            {
+                name: 'ArticleTest',
+                itemimg:TestImg ,
+                price: 8,
+                new: false,
+            },
+            {
+                name: 'ArticleTest',
+                itemimg:TestImg ,
+                price: 22,
+                new: false,
+            },
+            {
+                name: 'ArticleTest',
+                itemimg:TestImg ,
+                price: 22,
+                new: false,
+            },
+            {
+                name: 'ArticleTest',
+                itemimg:TestImg ,
+                price: 35,
+                new: false,
+            },
+            {
+                name: 'ArticleTest',
+                itemimg:TestImg ,
+                price: 174,
+                new: false,
+            },
+            {
+                name: 'ArticleTest',
+                itemimg:TestImg ,
+                price: 22,
+                new: false,
+            },
+        ]
     },
     {
         title:"Accessories",
         itemBig: true,
         bgimg:NavPic02,
         linkUrl: 'accessories',
+        items: [
+            {
+                name: 'ArticleTest',
+                itemimg:TestImg ,
+                price: 22,
+                new: false,
+            },
+            {
+                name: 'ArticleTest',
+                itemimg:TestImg ,
+                price: 22,
+                new: false,
+            },
+            {
+                name: 'ArticleTest',
+                itemimg:TestImg ,
+                price: 22,
+                new: false,
+            },
+            {
+                name: 'ArticleTest',
+                itemimg:TestImg ,
+                price: 22,
+                new: false,
+            },
+            {
+                name: 'ArticleTest',
+                itemimg:TestImg ,
+                price: 8,
+                new: false,
+            },
+            {
+                name: 'ArticleTest',
+                itemimg:TestImg ,
+                price: 22,
+                new: false,
+            },
+            {
+                name: 'ArticleTest',
+                itemimg:TestImg ,
+                price: 22,
+                new: false,
+            },
+            {
+                name: 'ArticleTest',
+                itemimg:TestImg ,
+                price: 35,
+                new: false,
+            },
+            {
+                name: 'ArticleTest',
+                itemimg:TestImg ,
+                price: 174,
+                new: false,
+            },
+            {
+                name: 'ArticleTest',
+                itemimg:TestImg ,
+                price: 22,
+                new: false,
+            },
+        ]
     },
     {
         title:"Others",
