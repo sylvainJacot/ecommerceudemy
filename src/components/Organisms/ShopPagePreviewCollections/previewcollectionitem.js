634ff755b468e0899f4be2b5c1c74731f196fb42
coatriver <sylvain.jacot@hotmail.be>
2020-01-20 23:58:50 +1100

    Shop page creation

---
+++ src/components/Organisms/ShopPagePreviewCollections/previewcollectionitem.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export class PreviewCollectionItem extends React.Component {
+    render() {
+        return (
+            <>
+                <p>Test</p>
+            </>
+        )
+    }
+}
--- src/components/Pages/shoppage.js
+++ src/components/Pages/shoppage.js
@@ -1,10 +1,11 @@
 import React from "react";
+import {PreviewCollections} from "../Organisms/ShopPagePreviewCollections/previewcollections";
 
 export class ShopPage extends React.Component {
     render() {
         return (
             <>
-                ShopPage
+                <PreviewCollections/>
             </>
         )
     }
